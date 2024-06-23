265c8f9ab8a33e0348f1e0c2850dafdc16838613
cerinetbls <cerine.tbls@gmail.com>
2024-06-23 19:29:00 +0200

    Ajout des données JSON et mise à jour du composant App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import logements from './logements.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Logements</h1>
+      <ul>
+        {logements.map(logement => (
+          <li key={logement.id}>{logement.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
