174ff89f395597a486f7ac3d99e4d7228591bd58
cerinetbls <cerine.tbls@gmail.com>
2024-06-29 16:05:49 +0200

    Création de la page A propos

---
+++ src/components/Collapse/Collapse.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './Collapse.scss';
+import flechUp from '../../assets/hightArrow.jpg';
+
+function Collapse({ title, content }) {
+    const [open, setOpen] = useState(false);
+
+    const handleToggle = (event) => {
+        // Vérifier si la cible du clic est l'image hightArrow
+        if (event.target.tagName === 'IMG') {
+            setOpen(!open);
+        }
+    };
+
+    return (
+        <div className='Collapse__container'>
+            <div className={`Collapse ${open ? "open" : ""}`}>
+                <div className='Collapse__title' onClick={handleToggle}>
+                    <h2>{title}</h2>
+                    <img
+                        src={flechUp}
+                        alt={open ? "Up" : "Down"}
+                        className={open ? "Collapse__title_img rotate" : "Collapse__title_img"}
+                    />
+                </div>
+                <div className='Collapse__content'>
+                    {open && (
+                        Array.isArray(content) ? (
+                            <ul>
+                                {content.map((item, index) => (
+                                    <li key={index}>{item}</li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>{content}</p>
+                        )
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Collapse;
--- src/pages/Apropos/Apropos.js
+++ src/pages/Apropos/Apropos.js
@@ -1,7 +1,57 @@
-import React from 'react';
+// import React from "react";
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Collapse from "../../components/Collapse/Collapse";
+import imgAproposBanner from "../../assets/paysageApropos.png";
+import './Apropos.scss';
+
+const DEFAULT_COLLAPSE = [
+    {
+        id: 0,
+        title: "Fiabilité",
+        content: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+    },
+    {
+        id: 1,
+        title: "Respect",
+        content: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+        id: 2,
+        title: "Service",
+        content: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+        id: 3,
+        title: "Sécurité",
+        content: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+];
 
 function Apropos() {
-  return <div>A Propos Page</div>;
+    // const [collapses, setCollapses] = useState(DEFAULT_COLLAPSE);
+
+    const CollapsesListe = DEFAULT_COLLAPSE.map((collapse) => (
+        <Collapse
+            key={collapse.id}
+            title={collapse.title}
+            content={collapse.content}
+        />
+    ));
+
+    return (
+        <div className="apropos">
+            
+                <Header />
+                <Banner texte="" image={imgAproposBanner} />
+                
+                <div className="Collapse__container">{CollapsesListe}</div>
+                <Footer />
+            
+        </div>
+    );
 }
 
 export default Apropos;
